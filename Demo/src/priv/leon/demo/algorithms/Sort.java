5699772124484ab6d205035cb75c4b8278bb3396
Leon <Leon_9408@163.com>
2018-05-23 14:29:04 +0800

    first commit Demo

---
+++ Demo/src/priv/leon/demo/algorithms/Sort.java
@@ -0,0 +1,41 @@
+package priv.leon.demo.algorithms;
+
+public class Sort {
+	public static void quickSort(int[]arr,int low,int high){
+		int l = low;
+		int h = high;
+		int key = arr[low];
+		while(l<h){
+			while(l<h&&arr[h]>=key)
+				h--;
+			if(l<h)
+				swap(arr,l,h);
+			while(l<h&&arr[l]<=key)
+				l++;
+			if(l<h)
+				swap(arr,l,h);
+		}
+		if(low<l)
+			quickSort(arr,low,l-1);
+		if(l<high)
+			quickSort(arr,l+1,high);
+	}
+	public static void swap(int[] arr,int i,int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	public static void print(int[]arr){
+		for (int i : arr) {
+			System.out.print(i+",");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int[]arr = {7,3,7,2,3,5,7,23,4,2,6,34,2,41,6,3,34,5,32,5,43,5,6,8};
+//		int[]arr = {1,8,5,4,6,2,8};
+		quickSort(arr,0,arr.length-1);
+		print(arr);
+		System.out.println("ok");
+	}
+}
